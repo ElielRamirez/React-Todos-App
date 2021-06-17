db2c35bcbd8f91c84b957d347ebe8f5ca7d6a1dc
Eliel Ignacio Ramirez Gamboa <eliel_ignacio_1@hotmail.com>
2021-06-17 15:41:07 -0600

    Primer commit de la app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
+import Preloader from './components/Preloader';
+import { createTodo, readTodos, updateTodo, deleteTodo } from './functions';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  const [todo, setTodo] = useState({ title:'', content:'' });
+  const [todos, setTodos] = useState(null);
+  const [currentId, setCurrentId] = useState(0);
 
-export default App;
+  useEffect( () => {
+      let currentTodo = currentId !== 0 ? todos.find( todo => todo._id === currentId) : { title: '', content: '' }
+      setTodo(currentTodo)
+      
+  },[currentId]);
+  
+  useEffect( () => {
+      const fetchData = async () => {
+          const result = await readTodos();
+          setTodos(result);
+      }
+      fetchData();
+  },[currentId]);
+
+  const clear = () =>{
+    setCurrentId(0);
+    setTodo( { title:'', content: '' } );
+  }
+
+  useEffect(()=>{
+    const clearField = (e) => {
+      if( e.keyCode === 27){
+        clear()
+      }
+    }
+    window.addEventListener('keydown',clearField)
+    return () => window.removeEventListener('keydown',clearField)
+  },[])
+    const onSubmitHandler = async (e) => {
+      e.preventDefault();
+      if(currentId === 0 ){
+        console.log('Create')
+        const result = await createTodo(todo);
+        setTodos([...todos, result])
+      }else{
+        await updateTodo(currentId,todo);
+      }
+      clear()
+    }
+    const removeTodo = async (id) => {
+      await deleteTodo(id);
+      const todosCopy = [...todos];
+      todosCopy.filter(todo => todo._id !== id);
+      setTodos(todosCopy);
+    }
+    return ( 
+      <div className = "container" >
+        <div className="row">
+           <pre>{JSON.stringify(todo)}</pre>
+           <pre></pre>
+          <form className="col s12" onSubmit={onSubmitHandler}>
+            <div className="row">
+              <div className="input-field col s6">
+                <i className="material-icons prefix"></i>
+                <input id="icon_prefix" type="text" className="validate" 
+                value={ todo.title}
+                onChange={e=>{setTodo({...todo, title:e.target.value})}} />
+                <label htmlFor="icon_prefix">Tilte</label>
+              </div>
+              <div className="input-field col s6">
+                <i className="material-icons prefix"></i>
+                <input id="icon_telephone" type="text" className="validate"
+                value={todo.content}
+                onChange={e=>{setTodo({...todo, content:e.target.value})}} />
+                <label htmlFor="icon_telephone">Description</label>
+              </div>
+            </div>
+            <div className="row right-align">
+                <button className="waves-effect waves-light btn">Submit</button>
+            </div>
+          </form>
+        </div>
+        {
+          !todos? <Preloader /> : todos.length>0 ? <ul className="collection">
+            {
+              todos.map(todo=>(
+                <li key={todo._id} onClick={()=>setCurrentId(todo._id)} className="collection-item">
+                  <div>
+                  <h5>{todo.title}</h5>
+                  <p>{todo.content}<a href="#!" onClick={() => removeTodo(todo._id)} className="secondary-content"><i className="material-icons">delete</i></a></p>
+                  </div>
+                </li>
+              ))
+            }</ul>
+            : <div>Nothing to do...</div>  
+        }
+        
+       
+
+      </div>);
+    }
+
+    export default App;
