6710da09511d00487079a58f05839825b60eb9ae
Eliel Ignacio Ramirez Gamboa <eliel_ignacio_1@hotmail.com>
2021-06-17 16:39:40 -0600

    eliminacion de pre  en frnt

--- src/App.js
+++ src/App.js
@@ -55,8 +55,7 @@ function App() {
     return ( 
       <div className = "container" >
         <div className="row">
-           <pre>{JSON.stringify(todo)}</pre>
-           <pre></pre>
+           
           <form className="col s12" onSubmit={onSubmitHandler}>
             <div className="row">
               <div className="input-field col s6">
